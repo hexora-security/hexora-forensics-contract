289e5712474d6432e33f625588347ac40e4033bc
devopscomputer <pauloti.engenharia@gmail.com>
2026-04-28 14:00:53 -0300

    chore: update hexora-web/artifacts/auditor/src/components/cyberpunk-bg/index.ts

--- hexora-web/artifacts/auditor/src/components/cyberpunk-bg/index.ts
+++
@@ -1,6 +0,0 @@
-export { CyberpunkBackground } from "./CyberpunkBackground";
-export type { CyberpunkBackgroundProps } from "./CyberpunkBackground";
-export { CyberpunkScene } from "./CyberpunkScene";
-export { Header, ScannerDemo } from "./Header";
-export { defaultConfig } from "./config";
-export type { CyberpunkConfig } from "./config";
